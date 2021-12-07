b519532255d30a8257805951dd6341662449b752
lanl0rd <k1ng@glass.earth>
2021-12-06 19:24:56 -0500

    publish

--- publish.sh
+++ publish.sh
@@ -1,8 +1,11 @@
 cd ../lan10rd/grams
 nx build docs-app-ng
 rsync -avhri --delete --progress dist/apps/docs-app-ng/* ../../lan10rd.github.io
+cd ../repos/lan10rd/lan10rd.github.io/assets
+python3 index.py
 cd ../../lan10rd.github.io
+cp ../lan10rd/grams/etc/github/publish.sh .
+cp ../repos/lan10rd/lan10rd.github.io/assets/index.json assets/index.json
 git add .
 git commit -m "publish"
 git push
-cp ../lan10rd/grams/etc/github/publish.sh .
